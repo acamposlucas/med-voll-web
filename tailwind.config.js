6c2eff0699dd460083fc1af8ee4cfe5fb1cc1db3
lucasalmeida <lac.lucas96@gmail.com>
2023-01-03 16:31:29 -0300

    Initial setup commit

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "src/pages/**/*.{js,ts,jsx,tsx}",
+    "src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        blue: {
+          900: "#00213D",
+          700: "#0B3B60",
+          500: "#339CFF",
+          200: "#85C4FF",
+        },
+        neutral: {
+          900: "#161717",
+          500: "#6B6E71",
+          400: "#90989F",
+          200: "#E7EBEF",
+        },
+        red: {
+          error: "#893131",
+        },
+      },
+    },
+  },
+  corePlugins: {
+    fontSize: false,
+  },
+  plugins: [require("tailwindcss-fluid-type")],
+};
